509629e208985ae20ae67bd748e3094e19c33596
Nick Thacke <ncthacke@gmail.com>
2023-07-12 14:32:20 -0400

    Initial Commit

---
+++ iDineAdmin/iDineAdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineAdminApp.swift
+//  iDineAdmin
+//
+//  Created by Nick Thacke on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct iDineAdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
